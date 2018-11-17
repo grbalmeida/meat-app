ec1f587d28144a40583d3a8cf1ff42f761f11943
gilvan <g.r.almeida@live.com>
2018-11-17 20:17:22 -0200

    Preloading all modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router'
+import { RouterModule, PreloadAllModules } from '@angular/router'
 
 import {ROUTES} from './app.routes'
 
@@ -36,7 +36,7 @@ import { SharedModule } from './shared/shared.module'
     BrowserModule,
     HttpModule,
     SharedModule.forRoot(),
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'pt-BR'}
