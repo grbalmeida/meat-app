d0c572e2088b182900e0c2f920d234ff4d93df7c
gilvan <g.r.almeida@live.com>
2018-11-17 17:04:57 -0200

    Creating custom validators

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms'
+import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms'
 import {Router} from '@angular/router'
 import {RadioOption} from '../shared/radio/radio-option.model'
 import { OrderService } from './order.service'
@@ -40,7 +40,22 @@ export class OrderComponent implements OnInit {
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('', 
         [Validators.required])
-    })
+    }, {validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key: string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+
+    if(!email || !emailConfirmation) {
+      return undefined
+    }
+
+    if(email.value !== emailConfirmation.value) {
+      return {emailsNotMatch: true}
+    }
+
+    return undefined
   }
 
   itemsValue(): number {
