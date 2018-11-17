d0a76777eb293455b4d7b9167208df2c3aedac55
gilvan <g.r.almeida@live.com>
2018-11-16 23:45:47 -0200

    Applying events in the shopping cart component

--- src/app/restaurants/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurants/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -21,4 +21,15 @@ export class ShoppingCartComponent implements OnInit {
     return this.shoppingCartService.total()
   }
 
+  clear() {
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(item: any) {
+    return this.shoppingCartService.removeItem(item)
+  }
+
+  addItem(item: any) {
+    this.shoppingCartService.addItem(item)
+  }
 }
