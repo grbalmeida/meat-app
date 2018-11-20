a42e775bf035d4d347b2d319a058380e714d46d3
gilvan <g.r.almeida@live.com>
2018-11-19 22:01:50 -0200

    Adding Jwt Token to Login Route

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,12 +1,17 @@
 import {Request, Response} from 'express'
 import {User, users} from './users' 
+import * as jwt from 'jsonwebtoken'
 
 export const handleAuthentication = (req: Request, resp: Response) => {
     const user: User = req.body
     if(isValid(user)) {
         const dbUser: User = users[user.email]
+        const token = jwt.sign(
+            {sub: dbUser.email, iss: 'meat-api'},
+            'meat-api-password'
+        )
         resp.json({
-            ...dbUser
+            ...dbUser, accessToken: token
         })
     } else {
         resp.status(403)
