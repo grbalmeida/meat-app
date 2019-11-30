038d61a488e69e4eef6e1ede9a207aa5f8275ae0
gilvan <g.r.almeida@live.com>
2019-11-30 18:16:00 -0300

    Update application dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule, LOCALE_ID, ErrorHandler } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, PreloadAllModules } from '@angular/router'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { LocationStrategy, HashLocationStrategy } from '@angular/common'
+import { LocationStrategy, HashLocationStrategy, registerLocaleData } from '@angular/common'
+import localePt from '@angular/common/locales/pt'
+
+registerLocaleData(localePt, 'pt')
 
 import { ROUTES } from './app.routes'
 
@@ -50,7 +53,7 @@ import { ApplicationErrorHandler } from './app.error-handler';
   ],
   providers: [
     { provide: LocationStrategy, useClass: HashLocationStrategy },
-    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: LOCALE_ID, useValue: 'pt' },
     { provide: ErrorHandler, useClass: ApplicationErrorHandler }
   ],
   bootstrap: [AppComponent]
